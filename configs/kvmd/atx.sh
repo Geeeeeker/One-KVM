7fffe6065ec6387081e8e07e2db80aef09043bad
Geeeeeker <104330220+Geeeeeker@users.noreply.github.com>
2026-04-05 20:55:24 +0800

    Update atx.sh

--- configs/kvmd/atx.sh
+++ configs/kvmd/atx.sh
@@ -20,16 +20,9 @@
 #                                                                            #
 # ========================================================================== #
 
-ATX=USBRELAY_HID
-echo $ATX
-case $ATX in
-    GPIO)
-    sudo /etc/kvmd/custom_atx/gpio.sh $1
-    ;;
-    USBRELAY_HID)
-    sudo /etc/kvmd/custom_atx/usbrelay_hid.sh $1
-    ;;
-    *)
-    echo "No thing."
-    exit -1
-esac
+#!/bin/bash
+# PiKVM ATX 入口转发
+
+ACTION=$1
+# 直接调用我们的串口逻辑脚本
+/etc/kvmd/custom_atx/gpio.sh "$ACTION"
