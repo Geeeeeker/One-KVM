c594f0591464279b5c2bebe6191e1de1fb2e3045
Geeeeeker <104330220+Geeeeeker@users.noreply.github.com>
2026-04-05 20:55:56 +0800

    Update gpio.sh

--- configs/kvmd/custom_atx/gpio.sh
+++ configs/kvmd/custom_atx/gpio.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
-# ========================================================================== #
-#                                                                            #
-#    KVMD - The main PiKVM daemon.                                           #
-#                                                                            #
-#    Copyright (C) 2023-2025  SilentWind <mofeng654321@hotmail.com>         #
-#                                                                            #
-#    This program is free software: you can redistribute it and/or modify    #
-#    it under the terms of the GNU General Public License as published by    #
-#    the Free Software Foundation, either version 3 of the License, or       #
-#    (at your option) any later version.                                     #
-#                                                                            #
-#    This program is distributed in the hope that it will be useful,         #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
-#    GNU General Public License for more details.                            #
-#                                                                            #
-#    You should have received a copy of the GNU General Public License       #
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
-#                                                                            #
-# ========================================================================== #
-case $1 in
+# RP2040 串口控制脚本
+
+DEV="/dev/ttyUSB0"
+BAUD="115200"
+
+# 初始化串口参数
+stty -F $DEV $BAUD -echo -icrnl -ixon -opost -onlcr
+
+case "$1" in
     short)
-    gpioset -m time -s 1 SHUTDOWNPIN=0
-    gpioset SHUTDOWNPIN=1
-    ;;
+        echo -e "short\n" > $DEV
+        ;;
     long)
-    gpioset -m time -s 5 SHUTDOWNPIN=0
-    gpioset SHUTDOWNPIN=1
-    ;;
+        echo -e "long\n" > $DEV
+        ;;
     reset)
-    gpioset -m time -s 1 REBOOTPIN=0
-    gpioset REBOOTPIN=1
-    ;;
-    *)
-    echo "No thing."
+        echo -e "reset\n" > $DEV
+        ;;
+    status)
+        # 清空缓存并读取状态
+        echo -e "status\n" > $DEV
+        # 读取一行返回，超时 0.5 秒
+        RES=$(timeout 0.5 head -n 1 $DEV | tr -d '\r\n')
+        if [ "$RES" == "ON" ]; then
+            exit 1 # 1 代表开机 (High)
+        else
+            exit 0 # 0 代表关机 (Low)
+        fi
+        ;;
 esac
+exit 0
+
